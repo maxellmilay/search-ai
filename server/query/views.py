d90283e96d499b578c61c1c4d46e622edf27924a
maxellmilay <milaymaxell@gmail.com>
2023-10-31 03:43:04 +0800

    [Feature][Maxell] integrated similarity search with llm

--- server/query/views.py
+++ server/query/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from pgvector.django import L2Distance
 from .models import LangchainPgEmbedding
 from .embeddings import get_embedding
+from .open_ai import answer_query
 
 @api_view(["POST"])
 def search_query(request):
@@ -20,10 +22,14 @@ def search_query(request):
             'query': query,
             'most_similar': most_similar.document
         }
+
+        answer = answer_query(context)
+        context['answer'] = answer
         
         return Response(context)
 
-    return Response({
-        "query": "There is no query",
-    })
+    return Response(
+        {"message": "There is no query"},
+        status=HTTP_400_BAD_REQUEST
+    )
     
